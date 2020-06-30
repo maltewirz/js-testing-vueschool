60b1dd4b591bbd51322b59b1a0a19188049b97e7
Malte Wirz <mail@maltewirz.com>
2020-06-30 15:49:11 +0200

    update unit test

--- 03-tdd/model.js
+++ 03-tdd/model.js
@@ -1,11 +1,22 @@
 export default class Model {
 
-    constructor() {
+    constructor(data = []) {
       this.$collection = []
+
+      if (data.length) {
+        this.record(data);
+      }
     }
   
-    record() { }
-    all() { }
+    record(data) {
+      this.$collection.push( ...data )
+    }
+    all() { 
+      // map will return a new array, Object.assign will merge 2 objects
+      // so this merges the entry with an empty object.
+      // therefore completely detached from original data
+      return this.$collection.map(entry => Object.assign({}, entry));
+    }
     update() { }
     find() { }
   }
--- 03-tdd/model.spec.js
+++ 03-tdd/model.spec.js
@@ -13,5 +13,43 @@ test('model structure', () => {
     update: expect.any(Function),
   }))
 })
-
 // $ prefix marks it as an internal property
+
+describe('record', () => {
+  const heroes = [{ name: 'Batman'}, { name: 'Black Panther' }];
+
+  test('can add data to the collection', () => {
+    const model = new Model();
+    model.record(heroes);
+    expect(model.$collection).toEqual(heroes);
+  });
+
+  test('gets called when data is passed to the model', () => {
+    const spy = jest.spyOn(Model.prototype, 'record');
+    const model = new Model(heroes);
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+});
+
+describe('all', () => {
+  const heroes = [{ name: 'Batman'}, { name: 'Black Panther' }];
+
+  test('return empty model', () => {
+    const model = new Model();
+    expect(model.all()).toEqual([]);
+  });
+
+  test('retrieve all data', () => {
+    const model = new Model(heroes);
+    expect(model.all().length).toEqual(2);
+  });
+
+  test('original data stays intact', () => {
+    const model = new Model(heroes);
+    const data = model.all();
+    data[0].name = 'Black Panther';
+    expect(model.$collection[0].name).toBe('Batman');
+
+  });
+});
